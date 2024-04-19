f533dcf485907a1bb97d1766880509cfdeb74541
mechanic <28224623+mechanic-kharkiv@users.noreply.github.com>
2024-04-19 03:27:44 +0200

    fbx_batch_import: joined version modules; v1.0.2;

--- fbx_batch_import/fbx_batch_import/__init__.py
+++ fbx_batch_import/fbx_batch_import/__init__.py
@@ -9,7 +9,7 @@ but with no tag adding. So, let it be. :-)
 Created on Jan 28, 2024
 
 @author: (c) LIX A.S. Mechanic.Kharkiv
-@last_edit: 2024-02-02
+@last_edit: 2024-04-19
 '''
 # DONE: sort out the version tag usage to keep it compatible with 2.7x and newer
 #   we just use 2.80: older complain, but allow it; the newer just accept.
@@ -19,7 +19,7 @@ Created on Jan 28, 2024
 bl_info = {
     "name": "FBX batch import with preset (*.fbx)",
     "author": "(c) LIX A.S. Mechanic.Kharkiv",
-    "version": (1, 0, 1),
+    "version": (1, 0, 2),
     "blender": (2, 80, 0),
     "location": "File > Import > FBX batch import",
     "description": "Import all fbx files from directory with named preset.",
@@ -30,17 +30,13 @@ bl_info = {
 import bpy
 
 from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.props import StringProperty, BoolProperty, EnumProperty, CollectionProperty
 from bpy.types import Operator
 
 import sys, os
 #import time
 
-from . import presets
-if bpy.app.version < (2,80,0):
-    from .classes27 import *
-else:
-    from .classes28 import *
+from .presets import *
 
 FBXPATH_TAG_NAME = "fbxpath"
 
@@ -54,7 +50,8 @@ def import_fbx(operator, path, preset_dict={}):
     operator.report({'DEBUG'}, mess)
     save_stdout = sys.stdout
     try:
-        sys.stdout = DummyFile() # suppress its output
+        if not operator.verbose:
+            sys.stdout = DummyFile() # suppress its output
         bpy.ops.import_scene.fbx(filepath=path, **preset_dict)
     finally:
         sys.stdout = save_stdout
@@ -125,14 +122,20 @@ def do_batch_import(operator, context, src_dir, fbx_import_preset_name="", use_t
 
         for new_object in new_objects:
             if (use_tags):
-                # add a custom property with source path
+                # add a custom property with the source path
                 new_object[FBXPATH_TAG_NAME] = fbx_path
+                new_object.data[FBXPATH_TAG_NAME] = fbx_path
             # register the new object
             objects_list.append(new_object)
             if new_object.type == 'MESH':
                 cnt_meshes += 1
             elif new_object.type == 'ARMATURE':
                 cnt_armatures += 1
+
+        if (use_tags):
+            for new_action in new_actions:
+                # fill the property with the source path
+                new_action.fbxpath = fbx_path
 
     mess = "Finished. Imported meshes [{}]; armatures [{}]; actions [{}]; from {} files.".format(
                     cnt_meshes, cnt_armatures, cnt_actions, cnt_files
@@ -150,13 +153,65 @@ def do_batch_import(operator, context, src_dir, fbx_import_preset_name="", use_t
 
 # ========= UI stuff ============
 
-class FbxBatchImport(Operator, FbxImportHelper, BatchImportOpProps):
+def make_annotations(cls):
+    """Add annotation attribute to fields to avoid Blender 2.8+ warnings"""
+    if not hasattr(bpy.app, "version") or bpy.app.version < (2, 80):
+        return cls
+    if bpy.app.version < (2, 93, 0):
+        bl_props = {k: v for k, v in cls.__dict__.items()
+                    if isinstance(v, tuple)}
+    else:
+        bl_props = {k: v for k, v in cls.__dict__.items()
+                    if isinstance(v, bpy.props._PropertyDeferred)}
+    if bl_props:
+        if '__annotations__' not in cls.__dict__:
+            setattr(cls, '__annotations__', {})
+        annotations = cls.__dict__['__annotations__']
+        for k, v in bl_props.items():
+            annotations[k] = v
+            delattr(cls, k)
+    return cls
+
+
+@make_annotations
+class FbxImportHelper(ImportHelper):
+    filter_glob = StringProperty(
+            default="*.fbx",
+            options={'HIDDEN'},
+            maxlen=1024,
+            )
+
+    files = CollectionProperty(
+            name="File Path",
+            type=bpy.types.OperatorFileListElement,
+            )
+
+    # needed for mix-ins
+    order = ["filepath", "filter_glob", "files"]
+
+
+@make_annotations
+class FbxBatchImport(FbxImportHelper, Operator):
     """Import all fbx files from directory with named preset."""
-    bl_idname = "import_scene_batch.fbx"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_idname = "import_scene_batch.fbx"
     bl_label = "FBX batch import (directory, preset)"
 
     # ImportHelper mixin class uses this
     filename_ext = ".fbx"
+
+    add_tags = BoolProperty(
+            name="Add 'fbxpath' tags",
+            description="Add fbxpath property to all imported objects",
+            default=True,
+            )
+
+    preset_name = EnumProperty(
+            name="Preset",
+            description="Previously saved preset for import options",
+            items=preset_list_callback,
+            )
+
+    verbose = BoolProperty(name="Verbose", default=False, description="Verbose console output")
 
     def execute(self, context):
         _preset =  self.preset_name if self.preset_name != presets.NO_PRESET else ""
@@ -166,8 +221,10 @@ class FbxBatchImport(Operator, FbxImportHelper, BatchImportOpProps):
         presets.rescan_preset_names("import_scene.fbx")
         return FbxImportHelper.invoke(self, context, event)
 
+
 def menu_func_import(self, context):
     self.layout.operator(FbxBatchImport.bl_idname, text=FbxBatchImport.bl_label)
+
 
 def register():
     bpy.utils.register_class(FbxBatchImport)
@@ -175,8 +232,11 @@ def register():
         bpy.types.INFO_MT_file_import.append(menu_func_import)
     else:
         bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    bpy.types.Action.fbxpath = StringProperty(description="imported from")
+
 
 def unregister():
+    del bpy.types.Action.fbxpath
     bpy.utils.unregister_class(FbxBatchImport)
     if bpy.app.version < (2,80,0):
         bpy.types.INFO_MT_file_import.remove(menu_func_import)
